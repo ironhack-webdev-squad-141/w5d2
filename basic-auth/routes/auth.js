554476a608122a41496a74fa6185752217ef2dcb
mjarraya <mjarraya@student.42.fr>
2019-07-02 15:16:38 +0200

    authorization done

--- basic-auth/routes/auth.js
+++ basic-auth/routes/auth.js
@@ -12,18 +12,39 @@ router.get("/signup", (req, res) => {
 router.post("/signup", (req, res) => {
   const { username, password } = req.body;
 
-  const salt = bcrypt.genSaltSync();
-  const hash = bcrypt.hashSync(password, salt);
+  if (!password || !username) {
+    res.render("auth/signup", { errorMessage: "Both fields are required" });
 
-  User.create({
-    username,
-    password: hash
-  })
-    .then(data => {
-      res.send(data);
+    return;
+  } else if (password.length < 8) {
+    res.render("auth/signup", {
+      errorMessage: "Password needs to be 8 characters min"
+    });
+
+    return;
+  }
+
+  User.findOne({ username: username })
+    .then(user => {
+      if (user) {
+        res.render("auth/signup", {
+          errorMessage: "This username is already taken"
+        });
+
+        return;
+      }
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
+
+      return User.create({
+        username,
+        password: hash
+      }).then(data => {
+        res.redirect("/");
+      });
     })
     .catch(err => {
-      res.send(err);
+      res.render("views/signup", { errorMessage: err._message });
     });
 });
 
